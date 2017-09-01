5d0fc90aa7cdef4531987b603758e3bb9106a9b3
Lukas Kleinschmidt <l.kleinschmidt@crolla-lowis.de>
2017-09-01 11:33:01 +0200

    Use passed event variable

--- fields/images/assets/js/script.js
+++ fields/images/assets/js/script.js
@@ -9,21 +9,21 @@
 			} else {
 				field.data( fieldname, true );
 			}
-			
-			
+
+
 			field.find("input.filter").on('input change', function() {
 	      filter = $(this).val();
 	      field.find(".images-dropdown .no-images-found").removeClass("da");
 	      if(filter != "") {
-	        
+
 	        $.expr[':'].Contains = function(a, i, m) {
 	         return $(a).text().toUpperCase()
 	                           .indexOf(m[3].toUpperCase()) >= 0;
-	        };	        
+	        };
 	        field.find(".images-dropdown a").addClass("filtered");
-	        	        
+
 	        field.find(".images-dropdown a .image:Contains('" + filter + "')").not("disabled").closest("a").removeClass("filtered");
-	        	        	        	        
+
 	        if (field.find(".images-dropdown a").not(".filtered").length === 0) {
 	          field.find(".images-dropdown .no-images-found").addClass("da");
 	        }
@@ -32,7 +32,7 @@
 	        field.find(".images-dropdown a").removeClass("filtered");
 	      }
 	    });
-	    
+
 	    field.find("input.filter").keydown(function(e){
 	        // UP
 	        if (e.keyCode == 38) {
@@ -43,7 +43,7 @@
 	            field.find(".images-dropdown a").removeClass("focused");
 	            field.find(".images-dropdown a").not(".filtered").not(".disabled").last().addClass("focused");
 	          }
-	        }    
+	        }
 	        // DOWN
 	        if (e.keyCode == 40) {
 	          if (field.find(".images-dropdown a.focused").length) {
@@ -61,36 +61,36 @@
 	          return false;
 	        }
 	    });
-			
-			
+
+
 			function noover() {
 			  field.find(".add").removeClass("over");
 			  field.find(".images-item").removeClass("over");
 			}
-			
+
 			function reset() {
-			  
+
 			  if (field.find(".images-item.selected").length) {
 			    field.find(".imagesgrid").addClass("filled");
 			  }
 			  else {
 			    field.find(".imagesgrid").removeClass("filled");
 			  }
-			  
+
 			  field.find(".images-dropdown").removeClass("open");
 			  field.find(".images-add-button").removeClass("open");
-			  			  
+
 			  if (field.find('.images-dropdown a').not(".disabled").length > 0) {
 			    field.find('.images-dropdown .no-more-images').removeClass("da");
 			  }
 			  else {
 			    field.find('.images-dropdown .no-more-images').addClass("da");
 			  }
-			  
+
 			};
-			
+
 			reset();
-			
+
 			function write() {
 			  field.find("input.images").val("").trigger('change');
 			  if (field.find(".images-item.selected").length > 1) {
@@ -99,7 +99,7 @@
 			      filenames.push($(this).data("image"));
 			    });
 			    filenames = "- " + filenames.join("\n- ");
-			    
+
 			    field.find("input.images").val(filenames).trigger('change');
 			  }
 			  else {
@@ -107,7 +107,7 @@
 			  }
 			  field.closest('form').trigger('keep');
 			}
-			
+
 			function select(filename) {
 			  var file = field.find(".images-item[data-image='" + filename + "']");
 			  file.insertBefore(field.find(".add")).addClass("selected");
@@ -116,7 +116,7 @@
 			  write();
 			  noover();
 			};
-			
+
 			function remove(filename) {
 			  field.find(".images-item[data-image='" + filename + "']").removeClass("selected");
 			  field.find(".images-dropdown a[data-filename='" + filename + "']").removeClass("disabled");
@@ -124,7 +124,7 @@
 			  noover();
 			  write();
 			};
-			
+
 			field.find(".images-item .btn.remove").on("click", function () {
 			  if (!$(this).is(".ui-sortable-helper .btn")) {
 			    var filename = $(this).closest(".images-item").data("image");
@@ -132,9 +132,9 @@
 			  }
 			  return false;
 			});
-			
+
 			field.find(".images-add-button").on("click", function(e) {
-			  event.stopPropagation();
+			  e.stopPropagation();
 			  field.find(".images-dropdown").toggleClass("open");
 			  field.find(".images-add-button").toggleClass("open");
 			  field.find("input.filter").focus();
@@ -148,7 +148,7 @@
 		      }
 			  });
 			});
-			
+
 			field.find(".images-dropdown a").on("click", function(e) {
 			  field.find("input.filter").val("");
 			  field.find("input.filter").trigger("change");
@@ -156,12 +156,12 @@
 		    field.find(".images-dropdown").removeClass("open");
 		    field.find(".images-add-button").removeClass("open");
 			});
-						
+
 			var files    = field.find('.imagesgrid');
 			var sortable = files.find('.sortable');
 			var items    = files.find('.images-item');
 			var api      = files.data('api');
-			
+
 			if(sortable.find('.images-item').length > 1) {
 			  sortable.sortable({
 			    tolerance: "pointer",
@@ -173,7 +173,7 @@
 			    }
 			  }).disableSelection();
 			}
-			
+
 			field.find('.field-content').droppable({
 			  tolerance: "pointer",
 			  hoverClass: 'over',
@@ -191,13 +191,13 @@
 			    field.find(".images-item").removeClass("over");
 			    var droppedImage = ui.draggable.data('helper');
 			    if (ui.draggable.hasClass("images-item")) {
-			      otherField = ui.draggable.closest(".field-with-images");			      
+			      otherField = ui.draggable.closest(".field-with-images");
 			      otherField.find(".images-dropdown a[data-filename='" + droppedImage + "']").removeClass("disabled");
 			      if (otherField.find(".selected").length <= 2) {
 		          otherField.find(".imagesgrid").removeClass("filled");
 		        }
 		        ui.draggable.removeClass("selected");
-		        
+
 		        otherField.find("input.images").val("");
 		        if (otherField.find(".images-item.selected").length > 1) {
 		          filenames = new Array();
@@ -205,14 +205,14 @@
 		            filenames.push($(this).data("image"));
 		          });
 		          filenames = "- " + filenames.join("\n- ");
-		          
+
 		          otherField.find("input.images").val(filenames);
 		        }
 		        else {
 		          otherField.find("input.images").val(otherField.find(".images-item.selected").data("image"));
 		        }
 		        otherField.closest('form').trigger('keep');
-		        
+
 				  }
 		      select(droppedImage);
 			  },
@@ -245,6 +245,6 @@
 
 		});
 	};
-	
+
 
 })(jQuery);
